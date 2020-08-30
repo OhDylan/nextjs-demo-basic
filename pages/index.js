20b2a69614ca03ef7934fadda3ed260ad1e8f12d
Oh Dylan <ddragon1128@hotmail.com>
2020-08-30 17:47:27 +0800

    moved next/ all together

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
 
 export default function Home() {
   return (
