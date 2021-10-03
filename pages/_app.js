da917103efc29a0324595a23bd0115a2eb2a777c
Oh Dylan <ddragon1128@hotmail.com>
2021-10-03 21:59:14 +0800

    Added NavBar to custom _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import '../styles/author.css'
+import NavBar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
