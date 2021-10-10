1b3e6635d226a3ea312cd3469e9e37135e175fd5
Oh Dylan <ddragon1128@hotmail.com>
2021-10-10 23:01:36 +0800

    Added head component to custom App compoenent

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import '../styles/author.css'
 import NavBar from '../components/NavBar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>My Next App</title>
+        <meta name='description' content='This is a desription for My Next App'/>
+      </Head>
       <NavBar />
       <Component {...pageProps} />
     </>
